c1b1ecec972bbe33c7d00cb9ad0fb1f3821ef5e3
m-shaka <buddha0818@gmail.com>
2021-01-30 19:12:18 +0900

    translate loop

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ pub fn parse(filepath: &String) -> Vec<char> {
     res
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum BfOpKind {
     IncPtr,
     DecPtr,
@@ -32,7 +32,7 @@ enum BfOpKind {
     WriteStdout,
     LoopSetToZero,
     LoopMovePtr,
-    LopMoveData,
+    LoopMoveData,
     JumpIfDataZero,
     JumpIfDataNotZero,
 }
@@ -40,21 +40,39 @@ enum BfOpKind {
 #[derive(Debug)]
 struct BfOp {
     pub kind: BfOpKind,
-    pub argument: usize,
+    pub argument: i32,
 }
 
 fn translate(insts: &[char]) -> Vec<BfOp> {
     let mut res: Vec<BfOp> = vec![];
-    let mut loopStack: Vec<usize> = vec![];
+    let mut loop_stack: Vec<usize> = vec![];
     let mut pc: usize = 0;
     let program_size = insts.len();
     while pc < program_size {
         let inst = insts[pc];
         match inst {
             '[' => {
+                loop_stack.push(res.len());
+                res.push(BfOp {
+                    kind: BfOpKind::JumpIfDataZero,
+                    argument: 0,
+                });
                 pc += 1;
             }
             ']' => {
+                let offset = loop_stack
+                    .pop()
+                    .expect(&format!("unmatched closing ']' at pc={}", pc));
+                let optimized_ops = optimize_loop(&res, offset);
+                if optimized_ops.len() == 0 {
+                    res[offset].argument = res.len() as i32;
+                    res.push(BfOp {
+                        kind: BfOpKind::JumpIfDataNotZero,
+                        argument: offset as i32,
+                    })
+                } else {
+                    res.splice(offset.., optimized_ops);
+                }
                 pc += 1;
             }
             _ => {
@@ -74,10 +92,75 @@ fn translate(insts: &[char]) -> Vec<BfOp> {
                 };
                 res.push(BfOp {
                     kind,
-                    argument: num_repeats,
+                    argument: num_repeats as i32,
                 })
             }
         }
+    }
+    res
+}
+
+fn optimize_loop(ops: &[BfOp], loop_start: usize) -> Vec<BfOp> {
+    let mut res: Vec<BfOp> = vec![];
+    let loop_size = ops.len() - loop_start;
+    match loop_size {
+        2 => {
+            let repeated_op = &ops[loop_start + 1];
+            match repeated_op.kind {
+                BfOpKind::IncData | BfOpKind::DecData => res.push(BfOp {
+                    kind: BfOpKind::LoopSetToZero,
+                    argument: 0,
+                }),
+                BfOpKind::IncPtr => res.push(BfOp {
+                    kind: BfOpKind::LoopMovePtr,
+                    argument: repeated_op.argument,
+                }),
+                BfOpKind::DecPtr => res.push(BfOp {
+                    kind: BfOpKind::LoopMovePtr,
+                    argument: -repeated_op.argument,
+                }),
+
+                _ => {}
+            }
+        }
+        5 => {
+            if ops[loop_start + 1].kind == BfOpKind::DecData
+                && ops[loop_start + 3].kind == BfOpKind::IncData
+                && ops[loop_start + 1].argument == 1
+                && ops[loop_start + 3].argument == 1
+            {
+                match (&ops[loop_start + 2], &ops[loop_start + 4]) {
+                    (
+                        BfOp {
+                            kind: BfOpKind::IncPtr,
+                            argument: a1,
+                        },
+                        BfOp {
+                            kind: BfOpKind::DecPtr,
+                            argument: a2,
+                        },
+                    ) if a1 == a2 => res.push(BfOp {
+                        kind: BfOpKind::LoopMoveData,
+                        argument: *a1,
+                    }),
+                    (
+                        BfOp {
+                            kind: BfOpKind::DecPtr,
+                            argument: a1,
+                        },
+                        BfOp {
+                            kind: BfOpKind::IncPtr,
+                            argument: a2,
+                        },
+                    ) if a1 == a2 => res.push(BfOp {
+                        kind: BfOpKind::LoopMoveData,
+                        argument: -*a1,
+                    }),
+                    _ => {}
+                }
+            }
+        }
+        _ => {}
     }
     res
 }
