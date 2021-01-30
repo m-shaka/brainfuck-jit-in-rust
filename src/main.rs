3c50cf9de9c4c1d359ac091c687a18aca459d56e
m-shaka <buddha0818@gmail.com>
2021-01-30 15:49:46 +0900

    define MachineCode struct

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,62 @@ extern crate libc;
 
 use std::mem;
 
-fn execute(insts: Vec<u8>) {
+const MASK: u8 = 0xFF;
+
+struct MachineCode {
+    content: Vec<u8>
+}
+
+impl MachineCode {
+    pub fn new() -> Self {
+        Self{content: vec!{}}
+    }
+
+    pub fn emit_bytes(&mut self, bs: &[u8]) {
+        self.content.append(&mut bs.to_vec());
+    }
+
+    pub fn emit_u16(&mut self, n: u16) {
+        self.emit_bytes(&[
+            n as u8 & MASK,
+            (n >> 8) as u8 & MASK
+        ])
+    }
+
+    pub fn emit_u32(&mut self, n: u32) {
+        self.emit_u16((n & MASK as u32) as u16);
+        self.emit_u16((n >> 16 & MASK as u32) as u16);
+    }
+
+    pub fn emit_u64(&mut self, n: u64) {
+        self.emit_u32((n & MASK as u64) as u32);
+        self.emit_u32((n >> 32 & MASK as u64) as u32);
+    }
+
+    pub fn get(&self) -> &Vec<u8> {
+            &self.content
+    }
+}
+
+fn execute(code: &Vec<u8>) {
     unsafe {
         let page = libc::mmap(
             std::ptr::null_mut(),
-            insts.len(),
+            code.len(),
             libc::PROT_EXEC | libc::PROT_READ | libc::PROT_WRITE,
             libc::MAP_ANONYMOUS | libc::MAP_PRIVATE,
             -1,
             0
         );
         let program: *mut u8 = mem::transmute(page);
-        program.copy_from_nonoverlapping(insts.as_ptr(), insts.len());
+        program.copy_from_nonoverlapping(code.as_ptr(), code.len());
         let f: fn() -> i64 = mem::transmute(page);
         f();
     }
 }
 fn main() {
-    let insts: Vec<u8> = vec!{
+    let mut code = MachineCode::new();
+    code.emit_bytes(&[
         0x48, 0xC7, 0xC0, 0x00, 0x00, 0x00, 0x00,
         0x48, 0xC7, 0xC7, 0x00, 0x00, 0x00, 0x00,
         0x4C, 0x89, 0xEE,
@@ -31,6 +69,6 @@ fn main() {
         0x48, 0xC7, 0xC2, 0x01, 0x00, 0x00, 0x00,
         0x0F, 0x05,
         0xc3
-    };
-    execute(insts)
+    ]);
+    execute(code.get())
 }
